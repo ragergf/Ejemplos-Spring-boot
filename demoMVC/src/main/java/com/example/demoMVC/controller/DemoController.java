47cae42876f2a641ee89c1de9d119657b0ed8b1b
Abraham Rager Gomez Flores <rager.gf@gmail.com>
2018-10-09 10:42:24 -0500

    Se crea demoMVC

---
+++ demoMVC/src/main/java/com/example/demoMVC/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.demoMVC.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DemoController {
+
+	@RequestMapping("/")
+	public String demo()
+	{
+		return "demo";
+	}
+	
+}
